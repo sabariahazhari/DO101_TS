a1cac3ce0999dbdc0b78968e32474f0b522247de
sabariahazhari <64153429+sabariahazhari@users.noreply.github.com>
2020-04-23 01:08:39 +0800

    Update myapps.js

--- myapps.js
+++ myapps.js
@@ -1,1 +1,3 @@
-console.log("React like master branchs!\n");
+console.log("Master Branch!\n");
+console.log("Already create feature1 branch by using github website and already learn to merge.\n");
+console.log("Creating feature2 branch from visual studio code.\n");
